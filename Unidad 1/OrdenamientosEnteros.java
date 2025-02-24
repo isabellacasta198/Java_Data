6744a8762aa8cbcfc4a9ab7a40000a71ae5d8494
isabellacasta198 <isabella.castano03@usc.edu.co>
2025-02-24 15:38:54 -0500

    OrdenamientosEnteros.java

--- Unidad 1/OrdenamientosEnteros.java
+++ Unidad 1/OrdenamientosEnteros.java
@@ -1,1 +1,58 @@
 
+public class OrdenamientosEnteros {
+
+    public static void main(String[] args) {
+        //declarar arreglo desordenado:
+        int[] desordenado = {15, 8, 7, 9, 5, 1, 0, 4, 10, 3};
+
+        //declarar arreglo que va a guardar los elementos ordenados:
+        int[] ordenado = insertSort(desordenado);
+
+        //Ordenar:
+        for (int i = 0; i < ordenado.length; i++) {
+            System.out.printf("%d,", ordenado[i]);
+        }
+
+    }
+
+    /**
+     */
+    public static int[] burbuja(int[] arr) {
+        int n = arr.length;
+        int[] ordenado = arr.clone(); // Copia del arreglo original
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (ordenado[j] > ordenado[j + 1]) {
+                    // Intercambio de valores
+                    int temp = ordenado[j];
+                    ordenado[j] = ordenado[j + 1];
+                    ordenado[j + 1] = temp;
+                }
+            }
+        }
+        return ordenado;
+    }
+
+    /**
+     * TERMINAR DE IMPLEMENTAR INSERTION SORT
+     *
+     * @param arr
+     * @return
+     */
+    public static int[] insertSort(int[] arr) {
+        int len = arr.length;
+
+        for (int i = 1; i < len; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && key < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+        return arr;
+    }
+
+}
